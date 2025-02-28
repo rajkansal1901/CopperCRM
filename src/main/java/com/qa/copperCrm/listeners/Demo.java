5ac7bed97b4186c4c7946c82faa668795a8994a4
Rajat Kansal <rajkansal1901@gmial.com>
2025-02-28 17:47:00 -0500

    added demo page

--- src/main/java/com/qa/copperCrm/listeners/Demo.java
+++ src/main/java/com/qa/copperCrm/listeners/Demo.java
@@ -2,4 +2,8 @@ package com.qa.copperCrm.listeners;
 
 public class Demo {
     String name = "rajat";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
