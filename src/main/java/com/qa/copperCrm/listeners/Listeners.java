1ee3ca823f8a91f8aa286db753cee5f9aadae12f
Rajat Kansal <rajkansal1901@gmial.com>
2025-03-04 07:49:25 -0500

    Update

--- src/main/java/com/qa/copperCrm/listeners/Listeners.java
+++ src/main/java/com/qa/copperCrm/listeners/Listeners.java
@@ -6,6 +6,6 @@ import org.testng.ITestResult;
 public class Listeners implements ITestListener {
 
     public void onTestFailure(ITestResult result) {
-
+        System.out.println("Hello");
     }
 }
